f72de3a397be8b43dc3140ba0037a85e175b3ff4
Mitchell Blom <mitchellblom@gmail.com>
2017-07-12 11:33:08 -0500

    first direction set complete

--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace hashsets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> Showroom = new HashSet<string>() {"Mustang", "Camero", "Charger", "BRZ"};
+
+            Console.WriteLine(Showroom.Count);
+
+            Showroom.Add("Mustang");
+
+            foreach (string car in Showroom) {
+                Console.WriteLine(car);
+            }
+
+            HashSet<string> UsedLot = new HashSet<string>() {"Spider", "350Z"};
+
+            Showroom.UnionWith(UsedLot);
+
+            Console.WriteLine("Union of both sets: ");
+            foreach (string car in Showroom) {
+                Console.WriteLine(car);
+            }
+
+            Showroom.Remove("BRZ");
+
+            Console.WriteLine("BRZ removed: ");
+            foreach (string car in Showroom) {
+                Console.WriteLine(car);
+            }
+
         }
     }
 }
