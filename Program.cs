6600c93475916232a5cbe457c38f727d3eaab785
Mitchell Blom <mitchellblom@gmail.com>
2017-07-12 12:14:27 -0500

    done

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace hashsets
     {
         static void Main(string[] args)
         {
+            // Showroom 
             HashSet<string> Showroom = new HashSet<string>() {"Mustang", "Camero", "Charger", "BRZ"};
 
             Console.WriteLine(Showroom.Count);
@@ -17,6 +18,7 @@ namespace hashsets
                 Console.WriteLine(car);
             }
 
+            // UsedLot
             HashSet<string> UsedLot = new HashSet<string>() {"Spider", "350Z"};
 
             Showroom.UnionWith(UsedLot);
@@ -32,6 +34,23 @@ namespace hashsets
             foreach (string car in Showroom) {
                 Console.WriteLine(car);
             }
+
+            // Junkyard
+            HashSet<string> Junkyard = new HashSet<string>() {"Accord", "Explorer", "Element", "Mustang", "Spider"};
+
+            HashSet<string> CommonCars = new HashSet<string>() {};
+
+            Showroom.IntersectWith(Junkyard);
+
+            Console.WriteLine("Intersecting models: ");
+            
+            foreach (string car in Showroom) {
+                Console.WriteLine(car);
+            }
+
+            Showroom.UnionWith(Junkyard);
+
+            Showroom.Remove("Element");
 
         }
     }
